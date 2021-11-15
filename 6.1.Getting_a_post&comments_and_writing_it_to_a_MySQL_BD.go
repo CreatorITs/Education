f528a5979db2aadf4cb8439c45a1951fc3651e99
CreatorITs <90754687+CreatorITs@users.noreply.github.com>
2021-11-15 15:37:43 +0200

    Update 6.1.Getting_a_post&comments_and_writing_it_to_a_MySQL_BD.go
    Функции созданные для работы с базой данных MySQL , приведены к универсальному и оптимальному виду.

--- 6.1.Getting_a_post&comments_and_writing_it_to_a_MySQL_BD.go
+++ 6.1.Getting_a_post&comments_and_writing_it_to_a_MySQL_BD.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
+	"time"
 )
 
 type Posts []struct {
@@ -22,89 +24,86 @@ type Сomments []struct {
 	Body   string `json:"body"`
 }
 
+var SqlDriverName = "mysql"
+var DB_Username = "root"
+var DB_Password = "root1234"
+var DB_Host = "127.0.0.1"
+var DB_Port = "3306"
+var HostUserPass = DB_Username + ":" + DB_Password + "@tcp" + "(" + DB_Host + ":" + DB_Port + ")/"
+
 func createDataBase(nameDB string) {
-	db, err := sql.Open("mysql", "root:root1234@tcp(127.0.0.1:3306)/")
+	db, err := sql.Open(SqlDriverName, HostUserPass)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	_,err = db.Exec("CREATE DATABASE "+nameDB)
+	_, err = db.Exec("CREATE DATABASE " + nameDB + ";")
 	if err != nil {
 		panic(err)
 	}
 }
 
-func createTablePosts(nameDB, nameTable string) {
-	db, err := sql.Open("mysql", "root:root1234@tcp(127.0.0.1:3306)/")
+func createTable(nameDB, nameTable string, columnNumber int, ColName1, ColName2, ColName3, ColName4, ColName5 string) {
+	var postsTable = "CREATE TABLE " + nameTable + "(" + ColName1 + " varchar(255)" + "," + ColName2 + " varchar(255)" + "," + ColName3 + " varchar(255)" + "," + ColName4 + " varchar(255)" + ")" + ";"
+	var commentsTable = "CREATE TABLE " + nameTable + "(" + ColName1 + " varchar(255)" + "," + ColName2 + " varchar(255)" + "," + ColName3 + " varchar(255)" + "," + ColName4 + " varchar(255)" + "," + ColName5 + " varchar(255)" + ")" + ";"
+
+	db, err := sql.Open(SqlDriverName, HostUserPass)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	_,err = db.Exec("USE "+nameDB)
+	_, err = db.Exec("USE " + nameDB + ";")
 	if err != nil {
 		panic(err)
 	}
 
-	_,err = db.Exec("CREATE TABLE "+nameTable+"(UserId integer, Id integer, Title varchar(255), Body varchar(255));")
-	if err != nil {
+	switch columnNumber  {
+	case 4:
+		_, err = db.Exec(postsTable)
+		if err != nil {
+			panic(err)
+		}
+	case 5:
+		_, err = db.Exec(commentsTable)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		fmt.Println("Set the number of columns (columnNumber) in the table with posts (TablePosts) to 4 and for the table with comments (TableComments) to 5")
 		panic(err)
 	}
 }
 
-func createTableComments(nameDB, nameTable string) {
-	db, err := sql.Open("mysql", "root:root1234@tcp(127.0.0.1:3306)/")
+func insertTable(nameDB, nameTable string, columnNumber int, UserId, PostId, Id, Title, Name, Email, Body interface{}) {
+	postsTable := "INSERT INTO " + nameTable + "(UserId, Id, Title, Body) VALUES (?, ?, ?, ?);"
+	commentsTable := "INSERT INTO " + nameTable + "(PostId, Id, Name, Email, Body) VALUES (?, ?, ?, ?, ?);"
+
+	db, err := sql.Open(SqlDriverName, HostUserPass)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	_,err = db.Exec("USE "+nameDB)
+	_, err = db.Exec("USE " + nameDB + ";")
 	if err != nil {
 		panic(err)
 	}
 
-	_,err = db.Exec("CREATE TABLE "+nameTable+"(PostId integer, Id integer, Name varchar(255), Email varchar(255),Body varchar(255));")
-	if err != nil {
-		panic(err)
-	}
-}
-
-func insertTablePosts(nameDB, nameTable string, UserId, Id, Title, Body interface{}) {
-	db, err := sql.Open("mysql", "root:root1234@tcp(127.0.0.1:3306)/")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	_,err = db.Exec("USE "+nameDB)
-	if err != nil {
-		panic(err)
-	}
-
-	ptable := "INSERT INTO "+nameTable+"(UserId, Id, Title, Body) VALUES (?, ?, ?, ?);"
-	_, err = db.Exec(ptable, UserId, Id, Title, Body)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func insertTableComments(nameDB, nameTable string, PostId, Id, Name, Email, Body interface{}) {
-	db, err := sql.Open("mysql", "root:root1234@tcp(127.0.0.1:3306)/")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	_,err = db.Exec("USE "+nameDB)
-	if err != nil {
-		panic(err)
-	}
-
-	ctable := "INSERT INTO "+nameTable+"(PostId, Id, Name, Email, Body) VALUES (?, ?, ?, ?, ?);"
-	_, err = db.Exec(ctable, PostId, Id, Name, Email, Body)
-	if err != nil {
+	switch columnNumber  {
+	case 4:
+		_, err = db.Exec(postsTable, UserId, Id, Title, Body)
+		if err != nil {
+			panic(err)
+		}
+	case 5:
+		_, err = db.Exec(commentsTable, PostId, Id, Name, Email, Body)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		fmt.Println("Set the number of columns (columnNumber) in the table with posts (TablePosts) to 4 and for the table with comments (TableComments) to 5")
 		panic(err)
 	}
 }
@@ -120,25 +119,25 @@ func jsonPlaceHolder(url string, webstruct interface{}) error {
 
 func main() {
 
-//Create DataBase and Tables
+	//Create DataBase and Tables for Posts and Comments
 	createDataBase("DB_UserID_7")
-	createTablePosts("DB_UserID_7", "TablePosts")
-	createTableComments("DB_UserID_7", "TableComments")
+	createTable("DB_UserID_7", "TablePosts", 4, "UserId", "Id", "Title", "Body", "Empty")
+	createTable("DB_UserID_7", "TableComments", 5, "PostId", "Id", "Name", "Email", "Body")
 
-
-// Posts struct
+	// 	Posts struct
 	var post Posts
 	urlpost := "https://jsonplaceholder.typicode.com/posts?userId=7"
 	jsonPlaceHolder(urlpost, &post)
 	for p := 0; p < len(post); p++ {
-		go insertTablePosts("DB_UserID_7", "TablePosts", post[p].UserId, post[p].Id, post[p].Title, post[p].Body)
+		go insertTable("DB_UserID_7", "TablePosts", 4, post[p].UserId, 0, post[p].Id, post[p].Title, 0, 0, post[p].Body)
 	}
 
-// Сomments struct
+	// Сomments struct
 	var comment Сomments
 	urlcomments := "https://jsonplaceholder.typicode.com/comments?postId=7"
 	jsonPlaceHolder(urlcomments, &comment)
 	for c := 0; c < len(comment); c++ {
-		go insertTableComments("DB_UserID_7", "TableComments", comment[c].PostId, comment[c].Id, comment[c].Name, comment[c].Email, comment[c].Body)
+		go insertTable("DB_UserID_7", "TableComments", 5, 0, comment[c].PostId, comment[c].Id, 0, comment[c].Name, comment[c].Email, comment[c].Body)
+		time.Sleep(1001 * time.Microsecond)
 	}
 }
